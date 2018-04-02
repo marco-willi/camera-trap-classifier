7ae5c9a663416399af58fc60f4eca52ae81829df
Marco Willi <will5448@umn.edu>
2018-04-02 12:55:41 +0200

    main file for tfr only creation

--- data_processing/tfr_splitter.py
+++ data_processing/tfr_splitter.py
@@ -108,7 +108,7 @@ class TFRecordSplitter(object):
         iterator = dataset_reader.get_iterator(
              self.files_to_split, batch_size=2048, is_train=False, n_repeats=1,
              output_labels=output_labels,
-             buffer_size=2048,
+             buffer_size=10192,
              decode_images=False,
              labels_are_numeric=False,
              max_multi_label_number=None,
