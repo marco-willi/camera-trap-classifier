564358f2fa24812894faeaa36bc11777cd922013
Ross Tyzack Pitman <rpitman@panthera.org>
2018-04-02 13:17:05 +0200

    updated configuration_data.py with west africa

--- training/configuration_data.py
+++ training/configuration_data.py
@@ -1,6 +1,68 @@
 
 def get_label_info(location='panthera', experiment="empty"):
 
+    if dataset == 'west_africa':
+        labels_all = {
+          'species': [
+            "Aardvark", "Antelope_Bates_Pygmy", "Baboon_Olive", "Badger_Honey",
+            "Bat", "Bird", "Blank", "Bluebill_Red_Headed",
+            "Bristlebill_Red_Tailed", "Buffalo_African", "Bushbuck", "Bushpig",
+            "Cat_African_Golden", "Cat_Domestic", "Cattle_Domestic",
+            "Chimpanzee_Eastern", "Civet_African", "Civet_African_Palm",
+            "Colobus_Uganda_Red", "Cusimanse_Alexanders", "Dog_Domestic",
+            "Duiker_Blue", "Duiker_Weyns", "Duiker_Yellow_Backed",
+            "Elephant_African", "Fire", "Galago", "Genet_Rusty_Spotted",
+            "Genet_Servaline", "Goat_Wild", "Guereza_Mantled",
+            "Hog_Giant_Forest", "Human", "Hyaena_Spotted", "Jackal_Side_Striped",
+            "Leopard_African", "Lion_African", "Mangabey_Grey_Cheeked",
+            "Mongoose_Banded", "Mongoose_Egyptian", "Mongoose_Slender",
+            "Mongoose_Water", "Mongouse_Banded", "Monitor_Nile", "Monkey_Blue",
+            "Monkey_LHoests", "Monkey_Red_Tailed", "Monkey_Vervet",
+            "Mouse_Congo_Forest", "Otter_Congo_Clawless",
+            "Pangolin_African_White_Bellied", "Pangolin_Giant",
+            "Porcupine_Crested", "Potto_Western", "Rat_Giant",
+            "Rat_Greater_Cane", "Reptile", "Rodent", "Serval", "Sitatunga",
+            "Snake", "Squirrel_African_Giant", "Tortoise_Common", "Vehicle",
+            "Waterbuck"]}
+
+        keep_labels = {
+          'species': [
+            "Aardvark", "Antelope_Bates_Pygmy", "Baboon_Olive", "Badger_Honey",
+            "Bat", "Bird", "Blank", "Bluebill_Red_Headed",
+            "Bristlebill_Red_Tailed", "Buffalo_African", "Bushbuck", "Bushpig",
+            "Cat_African_Golden", "Cat_Domestic", "Cattle_Domestic",
+            "Chimpanzee_Eastern", "Civet_African", "Civet_African_Palm",
+            "Colobus_Uganda_Red", "Cusimanse_Alexanders", "Dog_Domestic",
+            "Duiker_Blue", "Duiker_Weyns", "Duiker_Yellow_Backed",
+            "Elephant_African", "Fire", "Galago", "Genet_Rusty_Spotted",
+            "Genet_Servaline", "Goat_Wild", "Guereza_Mantled",
+            "Hog_Giant_Forest", "Human", "Hyaena_Spotted", "Jackal_Side_Striped",
+            "Leopard_African", "Lion_African", "Mangabey_Grey_Cheeked",
+            "Mongoose_Banded", "Mongoose_Egyptian", "Mongoose_Slender",
+            "Mongoose_Water", "Mongouse_Banded", "Monitor_Nile", "Monkey_Blue",
+            "Monkey_LHoests", "Monkey_Red_Tailed", "Monkey_Vervet",
+            "Mouse_Congo_Forest", "Otter_Congo_Clawless",
+            "Pangolin_African_White_Bellied", "Pangolin_Giant",
+            "Porcupine_Crested", "Potto_Western", "Rat_Giant",
+            "Rat_Greater_Cane", "Reptile", "Rodent", "Serval", "Sitatunga",
+            "Snake", "Squirrel_African_Giant", "Tortoise_Common", "Vehicle",
+            "Waterbuck"]}
+
+        label_mapping = None
+
+        if experiment == 'empty':
+            label_mapping = {'species': {x: 'Species' for x in
+                keep_labels['species'] if x not in ['Vehicle', 'Blank']}}
+            label_mapping['species']['Vehicle'] = 'Vehicle'
+            label_mapping['species']['Blank'] = 'Blank'
+
+        elif experiment == 'species':
+            keep_labels['species'].remove('Vehicle')
+            keep_labels['species'].remove('Blank')
+
+        else:
+            raise NotImplementedError("experiment: %s not implemented" % experiment)
+
     if location == 'southern_africa':
         labels_all = {
           'species': [
@@ -129,6 +191,110 @@ def get_label_info(location='panthera', experiment="empty"):
 
     else:
         raise NotImplementedError("location: %s not implemented" % location)
+
+    res = {'labels_all': labels_all, 'keep_labels': keep_labels,
+           'label_mapping': label_mapping}
+
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_label_info(dataset='panthera', type="empty"):
+
+    if dataset == 'panthera':
+        labels_all = {
+          'species': [
+            "Aardvark", "Antelope_Bates_Pygmy", "Baboon_Olive", "Badger_Honey",
+            "Bat", "Bird", "Blank", "Bluebill_Red_Headed",
+            "Bristlebill_Red_Tailed", "Buffalo_African", "Bushbuck", "Bushpig",
+            "Cat_African_Golden", "Cat_Domestic", "Cattle_Domestic",
+            "Chimpanzee_Eastern", "Civet_African", "Civet_African_Palm",
+            "Colobus_Uganda_Red", "Cusimanse_Alexanders", "Dog_Domestic",
+            "Duiker_Blue", "Duiker_Weyns", "Duiker_Yellow_Backed",
+            "Elephant_African", "Fire", "Galago", "Genet_Rusty_Spotted",
+            "Genet_Servaline", "Goat_Wild", "Guereza_Mantled",
+            "Hog_Giant_Forest", "Human", "Hyaena_Spotted", "Jackal_Side_Striped",
+            "Leopard_African", "Lion_African", "Mangabey_Grey_Cheeked",
+            "Mongoose_Banded", "Mongoose_Egyptian", "Mongoose_Slender",
+            "Mongoose_Water", "Mongouse_Banded", "Monitor_Nile", "Monkey_Blue",
+            "Monkey_LHoests", "Monkey_Red_Tailed", "Monkey_Vervet",
+            "Mouse_Congo_Forest", "Otter_Congo_Clawless",
+            "Pangolin_African_White_Bellied", "Pangolin_Giant",
+            "Porcupine_Crested", "Potto_Western", "Rat_Giant",
+            "Rat_Greater_Cane", "Reptile", "Rodent", "Serval", "Sitatunga",
+            "Snake", "Squirrel_African_Giant", "Tortoise_Common", "Vehicle",
+            "Waterbuck"],
+          'count': [
+
+            ]}
+
+        keep_labels = {
+          'species': [
+            "Aardvark", "Antelope_Bates_Pygmy", "Baboon_Olive", "Badger_Honey",
+            "Bat", "Bird", "Blank", "Bluebill_Red_Headed",
+            "Bristlebill_Red_Tailed", "Buffalo_African", "Bushbuck", "Bushpig",
+            "Cat_African_Golden", "Cat_Domestic", "Cattle_Domestic",
+            "Chimpanzee_Eastern", "Civet_African", "Civet_African_Palm",
+            "Colobus_Uganda_Red", "Cusimanse_Alexanders", "Dog_Domestic",
+            "Duiker_Blue", "Duiker_Weyns", "Duiker_Yellow_Backed",
+            "Elephant_African", "Fire", "Galago", "Genet_Rusty_Spotted",
+            "Genet_Servaline", "Goat_Wild", "Guereza_Mantled",
+            "Hog_Giant_Forest", "Human", "Hyaena_Spotted", "Jackal_Side_Striped",
+            "Leopard_African", "Lion_African", "Mangabey_Grey_Cheeked",
+            "Mongoose_Banded", "Mongoose_Egyptian", "Mongoose_Slender",
+            "Mongoose_Water", "Mongouse_Banded", "Monitor_Nile", "Monkey_Blue",
+            "Monkey_LHoests", "Monkey_Red_Tailed", "Monkey_Vervet",
+            "Mouse_Congo_Forest", "Otter_Congo_Clawless",
+            "Pangolin_African_White_Bellied", "Pangolin_Giant",
+            "Porcupine_Crested", "Potto_Western", "Rat_Giant",
+            "Rat_Greater_Cane", "Reptile", "Rodent", "Serval", "Sitatunga",
+            "Snake", "Squirrel_African_Giant", "Tortoise_Common", "Vehicle",
+            "Waterbuck"],
+            'count': [
+
+            ]}
+
+        label_mapping = None
+
+        if type == 'empty':
+            label_mapping = {'species': {x: 'species' for x in
+                keep_labels['species'] if x not in ['vehicle', 'blank']}}
+            label_mapping['species']['vehicle'] = 'vehicle'
+            label_mapping['species']['blank'] = 'blank'
+
+        elif type == 'species':
+            keep_labels['species'].remove('vehicle')
+            keep_labels['species'].remove('blank')
+
+        else:
+            raise NotImplementedError("type: %s not implemented" % type)
+
+    else:
+        raise NotImplementedError("dataset: %s not implemented" % dataset)
 
     res = {'labels_all': labels_all, 'keep_labels': keep_labels,
            'label_mapping': label_mapping}
