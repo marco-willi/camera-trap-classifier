8103e261a9469ac6f60ddfdd63dfd5861d7d48ce
Marco Willi <will5448@umn.edu>
2018-04-06 18:12:35 +0200

    changed doc

--- main_prediction.py
+++ main_prediction.py
@@ -3,7 +3,7 @@
 
     Usage example:
 
-    python3 main_predicting_cmd.py -image_dir /user/images/ \
+    python3 main_prediction.py -image_dir /user/images/ \
     -results_file /user/predictions/output.csv \
     -model_path /user/models/my_super_model.hdf5 \
     -class_mapping_json /user/models/label_mappings.json \
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-image_dir", type=str, required=True)
     parser.add_argument("-results_file", type=str, required=True)
-    parser.add_argument("-model_path", type=str,required=True)
+    parser.add_argument("-model_path", type=str, required=True)
     parser.add_argument("-class_mapping_json", type=str, required=True)
     parser.add_argument("-pre_processing_json", type=str, required=True)
     parser.add_argument("-export_file_type", type=str, default="csv",
@@ -35,7 +35,6 @@ if __name__ == '__main__':
     for k, v in args.items():
         print("Arg: %s, Value:%s" % (k, v))
 
-
     pred = Predictor(
         model_path=args['model_path'],
         class_mapping_json=args['class_mapping_json'],
@@ -49,7 +48,7 @@ if __name__ == '__main__':
 
 
 # Test command
-#python main_predicting_cmd.py -image_dir D:\\Studium_GD\\Zooniverse\\CamCatProject\\data\\sample_images_raw\\ \
+#python main_prediction.py -image_dir D:\\Studium_GD\\Zooniverse\\CamCatProject\\data\\sample_images_raw\\ \
 #-results_file D:\\Studium_GD\\Zooniverse\\CamCatProject\\data\\sample_images_raw\\preds.csv \
 #-model_path D:\\Studium_GD\\Zooniverse\\Data\\camtrap_trainer\\data\\southern_africa\\models\\species\\model_prediction_run_201804060404_incept_res_species.hdf5 \
 #-class_mapping_json D:\\Studium_GD\\Zooniverse\\Data\\camtrap_trainer\\data\\southern_africa\\models\\species\\label_mappings.json \
