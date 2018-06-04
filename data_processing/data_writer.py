e4f1fe1c988090fcff1dc6fe6adde34a3c3a7d98
Marco Willi <will5448@umn.edu>
2018-06-04 00:38:51 -0500

    slight adaptation of tfrecord formats, removed tfr splitting

--- data_processing/data_writer.py
+++ data_processing/data_writer.py
@@ -6,41 +6,37 @@ import logging
 import tensorflow as tf
 
 from pre_processing.image_transformations import read_jpeg
-from data_processing.data_inventory import DatasetInventory
+
+logger = logging.getLogger(__name__)
 
 
 class DatasetWriter(object):
     def __init__(self, tfr_encoder):
         self.tfr_encoder = tfr_encoder
 
-    def encode_inventory_to_tfr(
-         self, data_inventory, output_file,
+    def encode_to_tfr(
+         self, tfrecord_dict, output_file,
          image_pre_processing_fun=None,
          image_pre_processing_args=None,
          random_shuffle_before_save=True,
          overwrite_existing_file=True,
          prefix_to_labels=''):
-        """ Export Data Inventory to a TFRecord file """
+        """ Export TFRecord Dict to a TFRecord file """
 
         if os.path.exists(output_file) and not overwrite_existing_file:
-            logging.info("File: %s exists - not gonna overwrite" % output_file)
+            logger.info("File: %s exists - not gonna overwrite" % output_file)
             return None
 
-        logging.info("Starting to Encode Inventory to Dictionary")
+        logger.info("Starting to Encode Dict")
 
-        if not isinstance(data_inventory, DatasetInventory):
-            raise ValueError("data_inventory must be a DatasetInventory")
-
-        all_label_types = data_inventory.label_handler.get_all_label_types()
-        logging.info("Found following label types: %s" % all_label_types)
-
-        n_records = data_inventory.get_number_of_records()
-        logging.info("Found %s records in inventory" % n_records)
+        if not isinstance(tfrecord_dict, dict):
+            logger.error("tfrecord_dict must be a dictionary")
+            raise ValueError("tfrecord_dict must be a dictionary")
 
         # Create and Write Records to TFRecord file
         with tf.python_io.TFRecordWriter(output_file) as writer:
 
-            record_ids = data_inventory.get_all_record_ids()
+            record_ids = list(tfrecord_dict.keys())
 
             # Randomly shuffle records before saving, this is better for
             # model training
@@ -48,17 +44,18 @@ class DatasetWriter(object):
                 random.seed(123)
                 random.shuffle(record_ids)
 
-            logging.info("Start Writing Record to TFRecord - Total %s" %
-                         n_records)
+            n_records = len(tfrecord_dict.keys())
+            logger.info("Start Writing Records to TFRecord-File - Total %s" %
+                        n_records)
 
             # Loop over all records and write to TFRecord
             successfull_writes = 0
             for i, record_id in enumerate(record_ids):
 
                 if i % 1000 == 0:
-                    logging.info("Wrote %s / %s files" % (i, n_records))
+                    logger.info("Wrote %s / %s files" % (i, n_records))
 
-                record_data = data_inventory.get_record_id_data(record_id)
+                record_data = tfrecord_dict[record_id]
 
                 # Process all images in a record
                 raw_images = list()
@@ -72,36 +69,26 @@ class DatasetWriter(object):
                             image_raw = read_jpeg(image_path)
 
                     except Exception as e:
-                        logging.debug("Failed to read file: %s , error %s" %
-                                      (image_path, str(e)))
+                        logger.debug("Failed to read file: %s , error %s" %
+                                     (image_path, str(e)))
                         continue
 
                     raw_images.append(image_raw)
 
                 # check if at least one image is available
                 if len(raw_images) == 0:
-                    logging.info("Discarding record %s - no image avail" %
-                                 record_id)
-                    data_inventory.remove_record(record_id)
+                    logger.info("Discarding record %s - no image avail" %
+                                record_id)
                     continue
 
-                # add prefix to labels
-                label_data = {prefix_to_labels + k: v for k, v in
-                              record_data['labels'].items()}
+                record_data['images'] = raw_images
 
-                # Create Record to Serialize
-                record_to_serialize = dict()
-                record_to_serialize['id'] = record_id
-                record_to_serialize['labels'] = label_data
-                record_to_serialize['images'] = raw_images
-
-                serialized_record = self.tfr_encoder(record_to_serialize,
-                                                     labels_are_numeric=False)
+                serialized_record = self.tfr_encoder(record_data)
 
                 # Write the serialized data to the TFRecords file.
                 writer.write(serialized_record)
                 successfull_writes += 1
 
-            logging.info(
+            logger.info(
                 "Finished Writing Records to TFRecord - Wrote %s of %s" %
                 (successfull_writes, n_records))
