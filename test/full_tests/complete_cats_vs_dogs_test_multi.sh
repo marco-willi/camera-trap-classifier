2748d57e7227a19ce432e884d2ec96e81966f668
Marco Willi <will5448@umn.edu>
2018-07-29 02:37:33 -0500

    semi automate full tests

--- test/full_tests/complete_cats_vs_dogs_test_multi.sh
+++ test/full_tests/complete_cats_vs_dogs_test_multi.sh
@@ -1,30 +1,43 @@
-# Test Cats vs Dogs
+# Test Multi-Image Captures
+# ./test/full_tests/complete_cats_vs_dogs_test_multi.sh
+export_root_path=./test_big//cats_vs_dogs_multi/
+tfr_files_path=${export_root_path}tfr_files/
+run_outputs_path=${export_root_path}run_outputs/
+model_save_dir=${export_root_path}model_save_dir/
+run_outputs_tl_path=${export_root_path}run_outputs_tl/
+model_save_tl_dir=${export_root_path}model_save_dir_tl/
+
+# delete files
+rm $tfr_files_path/*
+rm $model_save_dir/*.hdf5
+rm $run_outputs_path/*
+rm $run_outputs_tl_path/*
 
 # Read from csv
 python create_dataset_inventory.py csv -path ./test/test_files/cats_vs_dogs_multi.csv \
--export_path ./test_big/cats_vs_dogs_multi/inventory.json \
+-export_path ${export_root_path}inventory.json \
 -capture_id_field id \
 -image_fields image \
 -label_fields species standing count
 
 # Create TFRecord Files
-python create_dataset.py -inventory ./test_big/cats_vs_dogs_multi/inventory.json \
--output_dir ./test_big/cats_vs_dogs_multi/tfr_files/ \
+python create_dataset.py -inventory ${export_root_path}inventory.json \
+-output_dir ${tfr_files_path} \
 -image_save_side_max 200 \
 -split_percent 0.7 0.15 0.15 \
 -overwrite
 
 # Train a Model
 python train.py \
--train_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-train_tfr_path ${tfr_files_path} \
 -train_tfr_pattern train \
--val_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-val_tfr_path ${tfr_files_path} \
 -val_tfr_pattern val \
--test_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-test_tfr_path ${tfr_files_path} \
 -test_tfr_pattern test \
--class_mapping_json ./test_big/cats_vs_dogs_multi/tfr_files/label_mapping.json \
--run_outputs_dir ./test_big/cats_vs_dogs/run_outputs/ \
--model_save_dir ./test_big/cats_vs_dogs/model_save_dir/ \
+-class_mapping_json ${tfr_files_path}label_mapping.json \
+-run_outputs_dir ${run_outputs_path} \
+-model_save_dir ${model_save_dir} \
 -model small_cnn \
 -labels species standing count \
 -labels_loss_weights 1 0.2 0.5 \
@@ -37,15 +50,15 @@ python train.py \
 
 # Transfer Learning
 python train.py \
--train_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-train_tfr_path ${tfr_files_path} \
 -train_tfr_pattern train \
--val_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-val_tfr_path ${tfr_files_path} \
 -val_tfr_pattern val \
--test_tfr_path ./test_big/cats_vs_dogs_multi/tfr_files \
+-test_tfr_path ${tfr_files_path} \
 -test_tfr_pattern test \
--class_mapping_json ./test_big/cats_vs_dogs_multi/tfr_files/label_mapping.json \
--run_outputs_dir ./test_big/cats_vs_dogs_multi/run_outputs_tl/ \
--model_save_dir ./test_big/cats_vs_dogs_multi/model_save_dir_tl/ \
+-class_mapping_json ${tfr_files_path}label_mapping.json \
+-run_outputs_dir ${run_outputs_tl_path} \
+-model_save_dir ${model_save_tl_dir} \
 -model small_cnn \
 -labels species standing \
 -batch_size 12 \
@@ -55,4 +68,4 @@ python train.py \
 -max_epochs 70 \
 -starting_epoch 0 \
 -transfer_learning \
--model_to_load ./test_big/cats_vs_dogs/run_outputs/
+-model_to_load ${run_outputs_path}
---
+++ test/full_tests/from_image_dir_test.sh
@@ -0,0 +1,131 @@
+# Test from image path
+# ./test/full_tests/from_image_dir_test.sh
+image_root_path=D:\\Studium_GD\\Zooniverse\\Data\\transfer_learning_project\\images\\4715\\all
+export_root_path=./test_big/cats_vs_dogs/
+tfr_files_path=${export_root_path}tfr_files/
+run_outputs_path=${export_root_path}run_outputs/
+model_save_dir=${export_root_path}model_save_dir/
+estimator_deploy_save_dir=${model_save_dir}estimator_deploy/
+estimator_save_dir=${model_save_dir}estimator/
+run_outputs_tl_path=${export_root_path}run_outputs_tl/
+model_save_tl_dir=${export_root_path}model_save_dir_tl/
+run_outputs_ft_path=${export_root_path}run_outputs_ft/
+model_save_ft_dir=${export_root_path}model_save_dir_ft/
+
+# delete files
+rm $tfr_files_path/*
+rm $model_save_dir/*.hdf5
+rm $run_outputs_path/*
+rm $run_outputs_tl_path/*
+rm $run_outputs_ft_path/*
+
+# Read from Class Directories
+python create_dataset_inventory.py dir -path $image_root_path \
+-export_path ${export_root_path}inventory.json
+
+# Create TFRecord Files
+python create_dataset.py -inventory ${export_root_path}inventory.json \
+-output_dir ${tfr_files_pat} \
+-image_save_side_max 200 \
+-split_percent 0.7 0.15 0.15 \
+-overwrite \
+-process_images_in_parallel \
+-process_images_in_parallel_size 2000 \
+-processes_images_in_parallel_n_processes 2
+
+# Train a Model
+python train.py \
+-train_tfr_path ${tfr_files_pat} \
+-train_tfr_pattern train \
+-val_tfr_path ${tfr_files_pat} \
+-val_tfr_pattern val \
+-test_tfr_path ${tfr_files_pat} \
+-test_tfr_pattern test \
+-class_mapping_json ${tfr_files_pat}label_mapping.json \
+-run_outputs_dir ${run_outputs_path} \
+-model_save_dir ${model_save_dir} \
+-model small_cnn \
+-labels class \
+-batch_size 128 \
+-n_cpus 2 \
+-n_gpus 1 \
+-buffer_size 512 \
+-max_epochs 6 \
+-starting_epoch 0
+
+
+# Continue Training
+python train.py \
+-train_tfr_path ${tfr_files_pat} \
+-train_tfr_pattern train \
+-val_tfr_path ${tfr_files_pat} \
+-val_tfr_pattern val \
+-test_tfr_path ${tfr_files_pat} \
+-test_tfr_pattern test \
+-class_mapping_json ${tfr_files_pat}label_mapping.json \
+-run_outputs_dir ${run_outputs_path} \
+-model_save_dir ${model_save_dir} \
+-model small_cnn \
+-labels class \
+-batch_size 128 \
+-n_cpus 2 \
+-n_gpus 1 \
+-buffer_size 512 \
+-max_epochs 7 \
+-starting_epoch 6 \
+-continue_training \
+-model_to_load ${run_outputs_path} \
+-color_augmentation full
+
+
+# Pseudo Transfer Training
+python train.py \
+-train_tfr_path ${tfr_files_pat} \
+-train_tfr_pattern train \
+-val_tfr_path ${tfr_files_pat} \
+-val_tfr_pattern val \
+-test_tfr_path ${tfr_files_pat} \
+-test_tfr_pattern test \
+-class_mapping_json ${tfr_files_pat}label_mapping.json \
+-run_outputs_dir ${run_outputs_tl_path} \
+-model_save_dir ${model_save_tl_dir} \
+-model small_cnn \
+-labels class \
+-batch_size 128 \
+-n_cpus 2 \
+-n_gpus 1 \
+-buffer_size 512 \
+-max_epochs 4 \
+-starting_epoch 0 \
+-transfer_learning \
+-model_to_load ${run_outputs_path} \
+
+
+# Pseudo Fine Tuning
+python train.py \
+-train_tfr_path ${tfr_files_pat} \
+-train_tfr_pattern train \
+-val_tfr_path ${tfr_files_pat} \
+-val_tfr_pattern val \
+-test_tfr_path ${tfr_files_pat} \
+-test_tfr_pattern test \
+-class_mapping_json ${tfr_files_pat}label_mapping.json \
+-run_outputs_dir ${run_outputs_ft_path} \
+-model_save_dir ${model_save_ft_dir} \
+-model small_cnn \
+-labels class \
+-batch_size 128 \
+-n_cpus 2 \
+-n_gpus 1 \
+-buffer_size 512 \
+-max_epochs 4 \
+-starting_epoch 0 \
+-fine_tuning \
+-model_to_load ${run_outputs_path} \
+
+# Deploy model
+python export.py -model ${model_save_dir}best_model.hdf5 \
+-class_mapping_json ${model_save_dir}label_mappings.json \
+-pre_processing_json ${model_save_dir}image_processing.json \
+-output_dir ${estimator_deploy_save_dir} \
+-estimator_save_dir ${estimator_save_dir}
