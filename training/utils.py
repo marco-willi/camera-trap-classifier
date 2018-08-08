semi-fix of accuracy calculation when missing labels

training/utils.py
@@ -2,8 +2,10 @@
 import csv
 import os
 
+import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.metrics import sparse_categorical_accuracy, sparse_top_k_categorical_accuracy
 
 from data.utils import copy_file
 
@@ -97,13 +99,23 @@ def masked_sparse_categorical_crossentropy(y_true, y_pred, mask_value=-1):
     return K.sparse_categorical_crossentropy(y_true * mask, y_pred * mask)
 
 
-def sparse_categorical_accuracy(y_true, y_pred):
-    return K.cast(K.equal(K.max(y_true, axis=-1),
-                          K.cast(K.argmax(y_pred, axis=-1), K.floatx())),
-                  K.floatx())
+def accuracy(y_true, y_pred, mask_value=-1):
+    mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
+    return sparse_categorical_accuracy(y_true * mask, y_pred * mask)
 
 
-def masked_accuracy(y_true, y_pred, mask_value=-1):
+def top_k_accuracy(y_true, y_pred, mask_value=-1):
+    mask = K.cast(K.not_equal(y_true, mask_value), K.floatx())
+    return sparse_top_k_categorical_accuracy(y_true * mask, y_pred * mask)
+
+# def masked_accuracy(y_true, y_pred, mask_value=-1):
+#     mask = tf.not_equal(y_true, tf.cast(mask_value, tf.float32))
+#     y_true_masked = tf.cast(tf.boolean_mask(y_true, mask, axis=0), tf.float32)
+#     y_pred_masked = tf.cast(tf.boolean_mask(y_pred, mask, axis=0), tf.float32)
+#     return sparse_categorical_accuracy(y_true_masked, y_pred_masked)
+
+
+def masked_accuracy2(y_true, y_pred, mask_value=-1):
     total = K.sum(K.cast(K.not_equal(y_true, mask_value), K.floatx()))
     correct = K.sum(K.cast(K.equal(y_true, K.round(y_pred)), K.floatx()))
     return correct / total