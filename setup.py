6dbb7410a2e39db171932f01da911692668d978d
Marco Willi <will5448@umn.edu>
2018-12-03 12:54:53 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='camera-trap-classifier',
+    url='https://github.com/marco-willi/camera-trap-classifier',
+    author='Marco Willi',
+    version='2.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'tensorflow==1.13',
+        'pyyaml',
+        'pillow'
+    ],
+    python_requires='>=3.5'
+)
