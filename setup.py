d63763f087ecd2ab323619231dbbe63f87c26856
Marco Willi <will5448@umn.edu>
2018-12-08 23:13:49 -0600

    increase version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='camera_trap_classifier',
     url='https://github.com/marco-willi/camera-trap-classifier',
     author='Marco Willi',
-    version='2.0.1',
+    version='2.1.0',
     packages=find_packages(),
     package_data={'': ['*.yaml']},
     include_package_data=True,
